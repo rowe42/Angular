8278d0636575d24bcade3d93bc261b23e5376b40
rowe42 <rolandwrn@gmail.com>
2017-03-17 10:20:53 +0100

    added http-calling part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+
+// Imports for loading & configuring the in-memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {DashboardComponent} from './dashboard.component';
+import {HeroSearchComponent} from './hero-search.component';
 
 import {HeroService} from './hero.service';
 
-import { AppRoutingModule }     from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule,
     ],
-    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent, HeroSearchComponent],
     providers: [HeroService],
     bootstrap: [AppComponent]
 })
